489caf82528fd730c2dcb5b3ab6a4f2f4bc70c7d
lcaparada <lcaparada@gmail.com>
2025-05-30 15:39:52 -0300

    feat: adding env and dynamic module

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,24 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { EnvConfigService } from './env-config.service';
+import { join } from 'node:path';
 
-@Module({
-  providers: [EnvConfigService],
-})
-export class EnvConfigModule {}
+@Module({})
+export class EnvConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    const envFile = join(__dirname, `../../../../.env.${process.env.NODE_ENV}`);
+
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: [envFile],
+          isGlobal: true,
+          ...options,
+        }),
+      ],
+      providers: [EnvConfigService],
+      exports: [EnvConfigService, ConfigModule],
+    };
+  }
+}
