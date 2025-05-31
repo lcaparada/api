448b065ccaaaf06f72c4d2adf8b4c939285d5482
lcaparada <lcaparada@gmail.com>
2025-05-31 13:35:32 -0300

    feat: create repository in memory

---
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -0,0 +1,43 @@
+import { Entity } from '../entities/entity';
+import { NotFoundError } from '../errors/not-found-error';
+import { RepositoryInterface } from './repository-contracts';
+
+export abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+    const index = this.items.findIndex((item) => item.id === entity.id);
+    this.items[index] = entity;
+  }
+
+  async findById(id: string): Promise<E> {
+    return this._get(id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this._get(id);
+    this.items.filter((item) => item.id === id);
+  }
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+
+  protected _get(id: string) {
+    const _id = `${id}`;
+    const entity = this.items.find((item) => item.id === _id);
+
+    if (!entity) {
+      throw new NotFoundError('Entity not found');
+    }
+
+    return entity;
+  }
+}
