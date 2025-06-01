19d535ec2f4f57ba62b2cc31e4a4583eb53bde34
lcaparada <lcaparada@gmail.com>
2025-06-01 14:38:18 -0300

    feat: create unit test inMemoryRepository update

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -31,7 +31,7 @@ describe('InMemoryRepository unit tests', () => {
       );
     });
 
-    it('Should return a entity by id', async () => {
+    it('Should return an entity by id', async () => {
       const entity = new StubEntity({ name: 'other name', price: 10 });
 
       await sut.insert(entity);
@@ -53,6 +53,31 @@ describe('InMemoryRepository unit tests', () => {
 
       expect(foundEntities).toBeDefined();
       expect(foundEntities).toContain(entity);
+    });
+  });
+
+  describe('Update', () => {
+    it('Should throw error when entity not found', async () => {
+      const entity = new StubEntity({ name: 'other name', price: 10 });
+
+      await expect(sut.update(entity)).rejects.toThrow(
+        new NotFoundError('Entity not found'),
+      );
+    });
+
+    it('Should update an entity', async () => {
+      const entity = new StubEntity({ name: 'other name', price: 10 });
+
+      await sut.insert(entity);
+
+      const updateEntity = new StubEntity(
+        { name: 'new name', price: 50 },
+        entity.id,
+      );
+
+      await sut.update(updateEntity);
+
+      expect(updateEntity.toJSON()).toStrictEqual(sut.items[0].toJSON());
     });
   });
 });
