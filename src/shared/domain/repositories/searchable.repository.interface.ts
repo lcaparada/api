1a9026a5eeedaba8054df3fd7a6a520230db5d47
lcaparada <lcaparada@gmail.com>
2025-06-02 09:07:23 -0300

    feat: adding search result class

--- src/shared/domain/repositories/searchable.repository.interface.ts
+++ src/shared/domain/repositories/searchable.repository.interface.ts
@@ -11,6 +11,16 @@ export type SearchProps<Filter = string> = {
   filter?: Filter | null;
 };
 
+export type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: SortDirection | null;
+  filter: Filter | null;
+};
+
 export class SearchParams {
   protected _page = 1;
   protected _perPage = 15;
@@ -18,7 +28,7 @@ export class SearchParams {
   protected _sortDir: SortDirection | null;
   protected _filter: string | null;
 
-  constructor(props: SearchProps) {
+  constructor(props: SearchProps<string>) {
     this.perPage = props.perPage;
     this.page = props.page;
     this.sort = props.sort;
@@ -87,6 +97,43 @@ export class SearchParams {
     }
     const dir = `${value}`.toLowerCase();
     this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir;
+  }
+}
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: SortDirection | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+
+  toJSON(forceEntity = false) {
+    return {
+      items: !forceEntity
+        ? this.items
+        : this.items.map((item) => item.toJSON()),
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    };
   }
 }
 
