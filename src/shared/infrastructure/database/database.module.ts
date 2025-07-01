cdb7353b189ce130a3c2546855a675ec017b674d
lcaparada <lcaparada@gmail.com>
2025-06-30 21:29:44 -0300

    feat: create database module

--- src/shared/infrastructure/database/database.module.ts
+++ src/shared/infrastructure/database/database.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule, Global } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { EnvConfigModule } from '../env-config/env-config.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
+@Global()
 @Module({
   providers: [PrismaService, ConfigService],
   imports: [EnvConfigModule.forRoot()],
   exports: [PrismaService],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forTest(prismaClient: PrismaClient): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: PrismaService,
+          useFactory: () => prismaClient as PrismaService,
+        },
+      ],
+    };
+  }
+}
